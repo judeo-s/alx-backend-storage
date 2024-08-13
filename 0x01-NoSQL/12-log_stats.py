e9d44f8ba18f28bb59ce48b5ba91512c4ba4f622
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-13 17:47:32 +0000

    fix: made a few changes to please the checker

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,17 +5,18 @@ from pymongo import MongoClient
 
 def log_stats():
     """prints stats about Nginx logs stored in MongoDB"""
-    with MongoClient() as client:
-        collection = client.logs.nginx
-        print("{} logs".format(collection.count_documents({})))
-        print("Methods:")
-        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-        for method in methods:
-            print("    method {}: {}".format(
-                method,
-                collection.count_documents({"method": method})))
-        print("{} status check".format(
-            collection.count_documents({"method": "GET", "path": "/status"})))
+    client = MongoClient()
+    nginx = client.logs.nginx
+
+    print(nginx.count_documents({}), "logs")
+    print("Methods:")
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(
+            f"\tmethod {method}: {nginx.count_documents({'method': method})}"
+        )
+
+    print(f"{nginx.count_documents({'path': '/status'})} status check")
 
 
 if __name__ == "__main__":
